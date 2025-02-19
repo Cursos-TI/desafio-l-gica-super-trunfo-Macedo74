545fd2ce167172ba2bc857b47dda9a4341bd4a9c
Valmir Macedo <valmirmacedo@gmail.com>
2025-02-19 02:53:57 +0000

    Novo Commit

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,38 +6,85 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
-
+    int PIB_A, PIB_B, Populacao_A, Populacao_B;
+    int area_A, area_B;
+    int areaTotal = 40000;
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    printf("Entre com o PIB da cidade A: \n");
+    scanf("%i", &PIB_A);
+    printf("Entre com a Populacao da cidade A: \n");
+    scanf("%i", &Populacao_A);
+    printf("Entre com a area da cidade A: \n");
+    scanf("%i", &area_A);
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    printf("Entre com o PIB da cidade B: \n");
+    scanf("%i", &PIB_B);
+    printf("Entre com a Populacao da cidade B: \n");
+    scanf("%i", &Populacao_B);
+    printf("Entre com a area da cidade B: \n");
+    scanf("%i", &area_B);
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    printf("\nCidade A: \n");
+    if (PIB_A < 100000000) {
+        printf("Cidade de Pequeno Porte. \n");
+    } else {
+        printf("Cidade de Grande Porte ou Metropole. \n");
+    }
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    if (Populacao_A > 500000) {
+        printf("Populacao de Grande Porte ou Metropole. \n");
+    } else {
+        printf("Populacao de Pequeno Porte. \n");
+    }
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    if (area_A < areaTotal) {
+        printf("Area de Cidade de Pequeno Porte. \n");
+    } else {
+        printf("Area de Cidade de Grande Porte ou Metropole. \n");
+    }
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    printf("\nCidade B: \n");
+    if (PIB_B < 100000000) {
+        printf("Cidade de Pequeno Porte. \n");
+    } else {
+        printf("Cidade de Grande Porte ou Metropole. \n");
+    }
+
+    if (Populacao_B > 500000) {
+        printf("Populacao de Grande Porte ou Metropole. \n");
+    } else {
+        printf("Populacao de Pequeno Porte. \n");
+    }
+
+    if (area_B < areaTotal) {
+        printf("Area de Cidade de Pequeno Porte. \n");
+    } else {
+        printf("Area de Cidade de Grande Porte ou Metropole. \n");
+    }
+
+    printf("\nA cidade vencedora é: \n");
+
+    if (PIB_A > PIB_B) {
+        printf("Cidade A, com maior PIB: %i\n", PIB_A);
+    } else if (PIB_B > PIB_A) {
+        printf("Cidade B, com maior PIB: %i\n", PIB_B);
+    } else {
+        // Caso os PIBs sejam iguais, podemos comparar a população
+        if (Populacao_A > Populacao_B) {
+            printf("Cidade A, com maior população: %i\n", Populacao_A);
+        } else if (Populacao_B > Populacao_A) {
+            printf("Cidade B, com maior população: %i\n", Populacao_B);
+        } else {
+            // Se população também for igual, comparamos a área
+            if (area_A > area_B) {
+                printf("Cidade A, com maior área: %i\n", area_A);
+            } else if (area_B > area_A) {
+                printf("Cidade B, com maior área: %i\n", area_B);
+            } else {
+                printf("As cidades são iguais em PIB, população e área.\n");
+            }
+        }
+    }
 
     return 0;
 }
